cd2b6cf8f14f76154b53be143ae77bb6988c584b
余多彬 <1510557673@qq.com>
2018-08-16 14:48:53 +0800

    ajax请求

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+  <div v-for="item  in  list"    :key="item.id">
+  <div class="footer-w1" style=" border-bottom: 0.1px   solid  #EFEFEF;"   >
+
+      <image src="./image/item-pic.png" class="footer-p1"
+             style=" width: 3.38rem ;height: 3.38rem;"/>
+
+      <div class="footer-w2  clearfix">
+
+        <p class="footer-text1">{{item.title}}</p>
+        <p class="footer-text2">{{item.description}}</p>
+        <p class="footer-text3">{{iteam.type}}</p>
+
+      </div>
+     </div>
+  </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+    list: []
+    };
+  },
+  created: function () {
+    this.getList();
+    console.log("启动中");
+  },
+  methods:{
+    getList(){
+      console.log("获取中");
+
+
+      this.$http.get('/api/lists?page_num=1&page_size=5', {}, {
+        headers: {
+
+        },
+        emulateJSON: true
+      }).then(function (res) {
+        this.list= res.body.data.rows;
+        console.log("成功了")
+        console.log(this.list)
+      }, function (error) {
+        console.log("失败了")
+        console.log(error)
+      })
+
+
+
+    }
+
+
+  }
+}
+</script>
+
+<style>
+  @import '../static/css/index.css';
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import VueResource from 'vue-resource'
+
+
+Vue.use(VueResource)
+
+
+/*使用VueResource插件*/
+
+
+new Vue({
+
+  el: '#app',
+  data: {
+
+  },
+  created: function () {
+
+  },
+  components: { App },
+  template: '<App/>',
+  methods: {}
+})
+
